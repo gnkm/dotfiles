a63eedee696fcff558b3f36f7b1f66b511845a6e
gnkm <genki.matsunaga@gmail.com>
2014-11-10 13:16:23 +0900

    add some keybinding configurations

---
+++ emacs/conf/30_keybindings.el
@@ -0,0 +1,6 @@
+;; C-t でウインドウを切り替える
+(global-set-key (kbd "C-t") 'other-window)
+;; C-h でバックスペース
+(global-set-key (kbd "C-h") 'delete-backward-char)
+;; C-? でヘルプ
+(global-set-key (kbd "C-x ?") 'help-command)
---
+++ emacs/conf/cocoa-emacs-30_keybindings.el
@@ -0,0 +1,2 @@
+;;メタキーをオプションキーに割り当て
+(setq mac-option-modifier 'meta)
