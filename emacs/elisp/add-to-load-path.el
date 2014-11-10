47ff011dfb2ed61d434eb6aface29660c8deb08d
gnkm <genki.matsunaga@gmail.com>
2014-11-10 09:51:25 +0900

    load-path を追加する関数の定義

---
+++ emacs/elisp/add-to-load-path.el
@@ -0,0 +1,11 @@
+;; load-path を追加する関数
+;; 参考：[[http://www.amazon.co.jp/dp/4774150029/][Emacs 実践入門]]
+(defun add-to-load-path (&rest paths)
+  (let (path)
+    (dolist (path paths paths)
+      (let ((default-directory
+              (expand-file-name (concat user-emacs-directory path))))
+        (add-to-list 'load-path default-directory)
+        (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+            (normal-top-level-add-subdirs-to-load-path))))))
+(provide 'add-to-load-path)
