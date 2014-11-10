6bed6020142c7c00538ed8902424eb4ee7286038
gnkm <genki.matsunaga@gmail.com>
2014-11-10 11:19:34 +0900

    add load-path

--- emacs/init.el
+++ emacs/init.el
@@ -4,3 +4,9 @@
 (cask-initialize)
 (require 'pallet)
 (pallet-mode t)
+
+;; add-to-load-path を使えるようにする
+(add-to-list 'load-path "~/.emacs.d/elisp")
+
+(require 'add-to-load-path)
+(add-to-load-path "public_repos")
