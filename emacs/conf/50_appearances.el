0a37e967712254713ca54c3cdff05dc9aa8d396b
gnkm <genki.matsunaga@gmail.com>
2014-11-10 13:33:51 +0900

    add some configurations for appearance

---
+++ emacs/conf/50_appearances.el
@@ -0,0 +1,16 @@
+;;; ===== ウインドウ =====
+
+;; 行番号を常に表示
+(global-linum-mode t)
+;; 対応する括弧を表示
+(show-paren-mode t)
+;; ediff 関連のバッファを 1つのフレームにまとめる
+(setq ediff-window-setup-function 'ediff-setup-window-plain)
+
+;;; ===== ミニバッファ =====
+
+;;; ===== モードライン =====
+
+;;; ===== ツールバー =====
+;; ツールバー非表示
+(tool-bar-mode -1)
