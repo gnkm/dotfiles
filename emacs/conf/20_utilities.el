62e9aecc211e3cf3d452f7e205b9a6703db66bae
gnkm <genki.matsunaga@gmail.com>
2014-11-21 21:31:14 +0900

    emacs: introduce helm

--- emacs/conf/20_utilities.el
+++ emacs/conf/20_utilities.el
@@ -1,8 +1,3 @@
-;; helm
-(require 'helm-config)
-(helm-mode 1)
-(define-key helm-read-file-map (kbd "<tab>") 'helm-execute-persistent-action)
-
 ;; auto-insert
 (auto-insert-mode)
 (setq auto-insert-directory "~/.emacs.d/insert-templates/")
