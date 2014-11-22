27594a1dd99403b5e4f8844786f35787d70943bf
gnkm <genki.matsunaga@gmail.com>
2014-11-22 11:35:14 +0900

    emacs: helm は最初から使えた方が便利なので eval-after-load は使わず普通 に require する

--- emacs/conf/30_helm.el
+++ emacs/conf/30_helm.el
@@ -1,17 +1,14 @@
 ;; http://tuhdo.github.io/helm-intro.html
+(require 'helm-config)
+(helm-mode 1)
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
+(define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
 (global-set-key (kbd "C-c h") 'helm-command-prefix)
 (global-set-key (kbd "C-x b") 'helm-mini)
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "M-y") 'helm-show-kill-ring)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
-
-(eval-after-load 'helm
-  '(progn
-     (require 'helm-config)
-     (helm-mode 1)
-     (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
-     (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
-     (define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
-     (global-set-key (kbd "C-c h o") 'helm-occur)
-     (global-set-key (kbd "C-c h g") 'helm-google-suggest)
-     (add-to-list 'helm-sources-using-default-as-input 'helm-source-man-pages)))
+(global-set-key (kbd "C-c h o") 'helm-occur)
+(global-set-key (kbd "C-c h g") 'helm-google-suggest)
+(add-to-list 'helm-sources-using-default-as-input 'helm-source-man-pages)
