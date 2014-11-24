c7814f7bc97239d38ca6eafee266c5b2be092c0d
gnkm <genki.matsunaga@gmail.com>
2014-11-24 20:57:49 +0900

    emacs: introduce yatex

--- emacs/conf/70_languages.el
+++ emacs/conf/70_languages.el
@@ -1,2 +1,12 @@
 ;; Shell script
 (define-auto-insert "\\.sh" "bash-template.sh")
+
+;; yatex
+(when (locate-library "yatex")
+  (add-to-list 'auto-mode-alist '("\\.tex$" . yatex-mode))
+  (autoload 'yatex-mode "yatex" "Yet Another LaTeX mode" t)
+  (setq YaTeX-inhibit-prefix-letter t)
+  (setq YaTeX-kanji-code nil)
+  (setq tex-command "platex")
+  (setq dvi2-command "evince")
+  (setq tex-pdfview-command "evince"))
--- emacs/init.el
+++ emacs/init.el
@@ -7,6 +7,11 @@
 (require 'pallet)
 (pallet-mode t)
 
+;; add-to-load-path を使えるように
+(add-to-list 'load-path "~/.emacs.d/elisp")
+(require 'add-to-load-path)
+(add-to-load-path "vendor")
+
 ;; init-loader
 (require 'init-loader)
 (init-loader-load "~/.emacs.d/conf")
