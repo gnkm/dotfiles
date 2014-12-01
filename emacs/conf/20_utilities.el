5cfbd46dadc9b734f97c65b837a7b8c828389cd1
gnkm <genki.matsunaga@gmail.com>
2014-12-01 18:53:06 +0900

    emacs: introduce flycheck

--- emacs/conf/20_utilities.el
+++ emacs/conf/20_utilities.el
@@ -1,8 +1,12 @@
 ;; C-k で行末の改行文字も削除する
 (setq kill-whole-line t)
+
 ;; auto-insert
 (auto-insert-mode)
 (setq auto-insert-directory "~/.emacs.d/insert-templates/")
+
+;; flycheck
+(add-hook 'after-init-hook #'global-flycheck-mode)
 
 ;; startup profiler
 (eval-after-load 'esup
