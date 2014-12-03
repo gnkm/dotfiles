f4ba7dcbc76b5e7e03be90cc61d3caada6ac321e
gnkm <genki.matsunaga@gmail.com>
2014-12-03 18:55:03 +0900

    emacs: change powerline colors

--- emacs/conf/65_powerline.el
+++ emacs/conf/65_powerline.el
@@ -2,18 +2,18 @@
 (require 'powerline)
 
 (set-face-attribute 'mode-line nil
-                    :foreground "#fff"
-                    :background "DarkOrange1"
+                    :foreground "white"
+                    :background "dark orange" ; #ff8c00
                     :box nil)
 
 (set-face-attribute 'powerline-active1 nil
-                    :foreground "#fff"
-                    :background "tomato3"
+                    :foreground "white"
+                    :background "DarkOrange4" ; #8b4500
                     :inherit 'mode-line)
 
 (set-face-attribute 'powerline-active2 nil
                     :foreground "white"
-                    :background "tomato4"
+                    :background "gray19" ; #303030
                     :inherit 'mode-line)
 
 (powerline-default-theme)
