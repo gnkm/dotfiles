f8f0555e72bf1b8f1aa3793967e7a8a0aa4e87a2
gnkm <genki.matsunaga@gmail.com>
2014-12-09 23:48:08 +0900

    emacs: introduce quelpa

---
+++ emacs/elisp/quelpa.el
@@ -0,0 +1,13 @@
+;; list of packages install from quelpa
+
+;; install quelpa
+;; evaluate the following expressions on scratch buffer
+
+;; (package-initialize)
+;; (if (require 'quelpa nil t)
+;;     (quelpa-self-upgrade)
+;;   (with-temp-buffer
+;;     (url-insert-file-contents "https://raw.github.com/quelpa/quelpa/master/bootstrap.el")
+;;     (eval-buffer)))
+
+;; list
