1672ea4a273bead86f6c53665c77703a2fb150a9
gnkm <genki.matsunaga@gmail.com>
2015-01-23 10:44:49 +0900

    emacs: edit twittering mode

--- emacs/conf/90_misc.el
+++ emacs/conf/90_misc.el
@@ -1,9 +1,6 @@
-;; twittering mode
-(autoload 'twit "twittering-mode")
-(eval-after-load 'twittering-mode
-    '(progn
-       (setq twittering-use-master-password t)
-       (setq twittering-initial-timeline-spec-string '(":home"))
-       (setq twittering-icon-mode t)	; Show icons
-       (setq twittering-timer-interval (* 5 60)) ; Update timeline each 5 minutes
-))
+;; ;; twittering mode
+(require 'twittering-mode)
+(setq twittering-use-master-password t)
+(setq twittering-initial-timeline-spec-string '(":home"))
+(setq twittering-icon-mode t)	; Show icons
+(setq twittering-timer-interval (* 5 60)) ; Update timeline each 5 minutes
