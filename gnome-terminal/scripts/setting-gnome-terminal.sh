49750a678a4c24523105e6d94feaed1e9d49d7de
gnkm <genki.matsunaga@gmail.com>
2015-04-18 15:18:13 +0900

    gnome-terminal: add example to change profile to zenburn

--- gnome-terminal/scripts/setting-gnome-terminal.sh
+++ gnome-terminal/scripts/setting-gnome-terminal.sh
@@ -4,6 +4,12 @@
 # ./setting-gnome-terminal.sh g cursor_blink_mode
 # ./setting-gnome-terminal.sh s cursor_blink_mode string 'off'
 
+# set zenburn theme
+## ./setting-gnome-terminal.sh s palette '#3F3F3F3F3F3F:#CCCC93939393:#7F7F9F9F7F7F:#E3E3CECEABAB:#DFDFAFAF8F8F:#CCCC93939393:#8C8CD0D0D3D3:#DCDCDCDCCCCC:#3F3F3F3F3F3F:#CCCC93939393:#7F7F9F9F7F7F:#E3E3CECEABAB:#DFDFAFAF8F8F:#CCCC93939393:#8C8CD0D0D3D3:#DCDCDCDCCCCC'
+## ./setting-gnome-terminal.sh s background_color '#3F3F3F3F3F3F'
+## ./setting-gnome-terminal.sh s foreground_color '#DCDCDCDCCCCC'
+## ./setting-gnome-terminal.sh s bold_color '#E3E3CECEABAB'
+
 # print the usage and exit
 print_usage_and_exit () {
 	cat <<____USAGE 1>&2
