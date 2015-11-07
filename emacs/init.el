b69c183377874177ddb26a139c06ce7e4dd279fb
gnkm <genki.matsunaga@gmail.com>
2015-11-07 21:47:20 +0900

    Emacs: Install use-package

--- emacs/init.el
+++ emacs/init.el
@@ -7,9 +7,15 @@
 (require 'pallet)
 (pallet-mode t)
 
+(require 'use-package)
+
 ;; add-to-load-path を使えるように
-(add-to-list 'load-path "~/.emacs.d/elisp")
-(require 'add-to-load-path)
+(use-package add-to-load-path
+	     :load-path "elisp")
+
+
+;; (add-to-list 'load-path "~/.emacs.d/elisp")
+;; (require 'add-to-load-path)
 (add-to-load-path "vendor")
 
 ;; init-loader
