b2ad542a5460d1070ae1fc17ce777730bb185915
gnkm <genki.matsunaga@gmail.com>
2015-11-08 16:24:30 +0900

    Emacs: Modify format of an el file

--- emacs/elisp/add-to-load-path.el
+++ emacs/elisp/add-to-load-path.el
@@ -1,5 +1,11 @@
+;;; add-to-load-path.el --- add load paths
+
+;;; Commentary:
 ;; load-path を追加する関数
 ;; 参考：[[http://www.amazon.co.jp/dp/4774150029/][Emacs 実践入門]]
+
+;;; Code:
+
 (defun add-to-load-path (&rest paths)
   (let (path)
     (dolist (path paths paths)
@@ -8,4 +14,6 @@
         (add-to-list 'load-path default-directory)
         (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
             (normal-top-level-add-subdirs-to-load-path))))))
+
 (provide 'add-to-load-path)
+;;; add-to-load-path.el ends here
