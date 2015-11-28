842d1c7472983d93de7e364f355a25f0145e52fc
gnkm <genki.matsunaga@gmail.com>
2015-11-29 08:22:25 +0900

    Emacs: Add an setting for org-refile

--- emacs/conf/_99_local.sample.el
+++ emacs/conf/_99_local.sample.el
@@ -9,6 +9,10 @@
 (setq org-agenda-files
       '("~/dropbox/org/inbox.txt"))
 
+(setq org-refile-targets
+      (quote ((nil :maxlevel . 9)
+	      (org-agenda-files :maxlevel . 2))))
+
 (setq org-capture-templates
              '(("i" "Inbox" plain (file "~/dropbox/org/inbox.txt")
 		"** %? %U")))
