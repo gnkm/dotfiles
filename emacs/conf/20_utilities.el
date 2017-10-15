ed0879824fd585b620524888eaca49bdaab28fdd
gnkm <genki.matsunaga@gmail.com>
2017-10-15 09:39:45 +0900

    Emacs: Delete conf for ddskk

--- emacs/conf/20_utilities.el
+++ emacs/conf/20_utilities.el
@@ -12,16 +12,6 @@
 
 (use-package auto-install)
 
-;; ddskk
-(use-package skk
-  :config
-  (global-set-key "\C-\\" 'skk-mode)
-  (setq skk-use-azik t)
-  (setq-default skk-kutouten-type 'en)
-  (setq skk-tut-file (concat skk-user-directory "SKK.tut"))
-  (setq skk-henkan-face (skk-make-face 'grey/DimGray))
-  (setq skk-large-jisyo (concat skk-user-directory "dic/SKK-JISYO.L")))
-
 ;; flycheck
 (add-hook 'after-init-hook #'global-flycheck-mode)
 
