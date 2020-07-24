27f5ff839cf9ba21daf168ceebaeb5091b4ec6c8
Genki Matsunaga <genki.matsunaga@gmail.com>
2020-07-24 16:48:13 +0900

    Edit setup script for mac: Delete unnecessary lines

--- setup-scripts/os/mac.sh
+++ setup-scripts/os/mac.sh
@@ -1,7 +1,5 @@
 #! /usr/bin/env bash
 
-$HOME/.dotfiles/setup-scripts/apps/atom.sh mac
-$HOME/.dotfiles/setup-scripts/apps/emacs.sh
 $HOME/.dotfiles/setup-scripts/apps/git.sh
 $HOME/.dotfiles/setup-scripts/apps/tmux.sh
-$HOME/.dotfiles/setup-scripts/apps/zsh.sh mac
+$HOME/.dotfiles/setup-scripts/apps/vscode.sh
