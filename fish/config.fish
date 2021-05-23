efca74c7449656cf3ef1369ecaf3121b350a5957
Genki Matsunaga <genki.matsunaga@gmail.com>
2021-05-23 21:13:03 +0900

    Fish: Change indent size

--- fish/config.fish
+++ fish/config.fish
@@ -1,44 +1,44 @@
 # PATH
 # linux brew
 if test -d $HOME/../linuxbrew/.linuxbrew/bin
-  set -x PATH $HOME/../linuxbrew/.linuxbrew/bin/ $PATH
+    set -x PATH $HOME/../linuxbrew/.linuxbrew/bin/ $PATH
 end
 
 # cask
 if test -d $HOME/.cask/bin
-  set -x PATH $HOME/.cask/bin $PATH
+    set -x PATH $HOME/.cask/bin $PATH
 end
 
 # cabal
 if test -d $HOME/.cabal/bin
-  set -x PATH $HOME/.cabal/bin $PATH
+    set -x PATH $HOME/.cabal/bin $PATH
 end
 
 # directory of src developed with go lang
 set -x GOPATH $HOME/projects
 if test -d $GOPATH
-  set -x PATH $GOPATH/bin $PATH
+    set -x PATH $GOPATH/bin $PATH
 end
 
 # *env
 # nodenv
 if test -d $HOME/.anyenv/envs/nodenv
-  set -x PATH $HOME/.anyenv/envs/nodenv/bin $PATH
-  status --is-interactive; and source (nodenv init -|psub)
+    set -x PATH $HOME/.anyenv/envs/nodenv/bin $PATH
+    status --is-interactive; and source (nodenv init -|psub)
 
-  # Second line command is faster than first one.
-  # set -x PATH $HOME/.nodenv/versions/**/bin $PATH
-  set -x PATH $HOME/.nodenv/versions/12.18.2/bin $PATH
+    # Second line command is faster than first one.
+    # set -x PATH $HOME/.nodenv/versions/**/bin $PATH
+    set -x PATH $HOME/.nodenv/versions/12.18.2/bin $PATH
 end
 
 # pyenv
 if which pyenv > /dev/null
-  status --is-interactive; and source (pyenv init -|psub)
+    status --is-interactive; and source (pyenv init -|psub)
 end
 
 # pyenv-virtualenv
 if which pyenv-virtualenv-init > /dev/null
-  status --is-interactive; and source (pyenv-virtualenv-init -|psub)
+    status --is-interactive; and source (pyenv-virtualenv-init -|psub)
 end
 
 # pip
@@ -46,8 +46,8 @@ set -x PATH $HOME/Library/Python/2.7/bin $PATH
 
 # rbenv
 if test -d $HOME/.rbenv
-  status --is-interactive; and source (rbenv init -|psub)
-  set -x PATH $HOME/.rbenv/shims $PATH
+    status --is-interactive; and source (rbenv init -|psub)
+    set -x PATH $HOME/.rbenv/shims $PATH
 end
 
 # fisher
@@ -74,11 +74,11 @@ alias pip pip3
 # Unix Commands
 switch (uname -s)
 case 'Linux'
-  abbr la 'ls -la --time-style="+%Y-%m-%d"'
+    abbr la 'ls -la --time-style="+%Y-%m-%d"'
 case 'Darwin'
-  if which gls > /dev/null
-    abbr la 'gls -la --time-style="+%Y-%m-%d"'
-  end
+    if which gls > /dev/null
+        abbr la 'gls -la --time-style="+%Y-%m-%d"'
+    end
 end
 
 # datefymd を使ってその日のコミット一覧を表示する
