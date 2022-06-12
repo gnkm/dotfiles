880616b6fc0d39ce7565ab29fd4816a6f41f5244
gnkm <genki.matsunaga@gmail.com>
2022-06-13 02:23:49 +0900

    Fish: Add a script file for setting fish

---
+++ setup-scripts/apps/fish.sh
@@ -0,0 +1,4 @@
+#! /usr/bin/env bash
+
+ln -s $HOME/.dotfiles/fish/config.fish $HOME/.config/fish/config.fish
+ln -s $HOME/.dotfiles/fish/completions/poetry.fish $HOME/.config/fish/completions/poetry.fish
