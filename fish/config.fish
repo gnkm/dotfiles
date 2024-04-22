38ed7a43eb863772d4919dcddaf4eef6cd84bd1b
gnkm <genki.matsunaga@gmail.com>
2024-04-22 18:56:27 +0900

    Fish: Install conda

--- fish/config.fish
+++ fish/config.fish
@@ -146,3 +146,16 @@ abbr dice 'random 1'
 if not env | grep SSH_AGENT_PID --silent
     eval (ssh-agent -c)
 end
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /opt/anaconda3/bin/conda
+    eval /opt/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/opt/anaconda3/etc/fish/conf.d/conda.fish"
+        . "/opt/anaconda3/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/opt/anaconda3/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
