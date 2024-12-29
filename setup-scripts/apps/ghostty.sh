798132e2657ace7066c3ae38e2313c14a4dce8e5
gnkm <genki.matsunaga@gmail.com>
2024-12-29 20:10:09 +0900

    Ghostty: Add some settings

---
+++ setup-scripts/apps/ghostty.sh
@@ -0,0 +1,4 @@
+#! /usr/bin/env bash
+
+ln -s $HOME/.dotfiles/ghostty/config $HOME/.config/ghostty/config
+ln -s $HOME/.dotfiles/ghostty/themes/my-zenburn.conf $HOME/.config/ghostty/themes/my-zenburn
