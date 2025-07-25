7348fd0cdb85098cc4835b765c88528243b2b687
gnkm <genki.matsunaga@gmail.com>
2025-07-25 12:06:02 +0900

    Zsh: 設定追加

--- setup-scripts/apps/zsh.sh
+++ setup-scripts/apps/zsh.sh
@@ -1,24 +1,3 @@
 #! /usr/bin/env bash
 
-OS=$1
-ZSH_DIR=$HOME/.zsh
-
-mkdir $ZSH_DIR
-# z の履歴ファイル作成
-touch $ZSH_DIR/.z
-
-ln -s $HOME/.dotfiles/zsh/common/.zshrc $HOME/.zshrc
-ln -s $HOME/.dotfiles/zsh/common/.zshrc-aliases $ZSH_DIR/common-aliases
-ln -s $HOME/.dotfiles/zsh/common/.zshrc-zplug $ZSH_DIR/common-zplug
-ln -s $HOME/.dotfiles/zsh/common/.zshrc-functions $ZSH_DIR/common-functions
-ln -s $HOME/.dotfiles/zsh/common/.dircolors.zenburn $ZSH_DIR/.dircolors
-
-if [[ -s $HOME/.dotfiles/zsh/$OS/.zshrc ]]; then
-    ln -s $HOME/.dotfiles/zsh/$OS/.zshrc $ZSH_DIR/os-specific-zshrc
-fi
-
-if [[ $OS = 'mac' ]]; then
-    ln -s $HOME/.dotfiles/zsh/$OS/.zshrc-aliases-coreutils $ZSH_DIR/coreutils-aliases
-fi
-
-ln -s $HOME/.dotfiles/zsh/$OS/.zshrc-aliases $ZSH_DIR/os-specific-aliases
+ln -s $HOME/.dotfiles/zsh/.zshrc $HOME/.zshrc
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -1,3 +1,29 @@
-eval "$(sheldon source)"
+if command -v sheldon &> /dev/null; then
+  eval "$(sheldon source)"
+fi
 
-eval "$(starship init zsh)"
+if command -v starship &> /dev/null; then
+  eval "$(starship init zsh)"
+fi
+
+if command -v zoxide &> /dev/null; then
+  eval "$(zoxide init zsh)"
+fi
+
+if type abbr &> /dev/null; then
+  ## eza
+  abbr la='eza -al --time-style long-iso'
+  abbr ll='eza -l --time-style long-iso'
+  abbr lt='eza -I ".git" -aTL 2'
+  ## git
+  abbr ga='git add'
+  abbr gan='git add . -n'
+  abbr gbl='git blame'
+  abbr gbr='git branch'
+  abbr gcam='git commit -am'
+  abbr gcm='git commit'
+  abbr gcmm='git commit -m'
+  abbr gco='git checkout'
+  abbr gd='git diff'
+  abbr gf='git fetch'
+fi
